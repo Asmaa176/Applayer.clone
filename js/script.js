faf77b27063d954fbf64bf19b4f2dedca2a0d503
Asmaa176 <asmaamohmady21@gmail.com>
2018-10-08 16:38:06 +0200

    initil commit by asmaa

---
+++ js/script.js
@@ -0,0 +1,11 @@
+﻿// $(document).ready(function(){
+//     $("header.nav-btn").click(function(){
+//         $(this).parent().toggleClass("active");
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("header.nav-btn").click(function(){
+//         $("ul").toggleClass("active");
+//     });
+// });
